4072275cae33ead42eb63ce750571335c73b7127
CasimiroDev <casimiro1099@gmail.com>
2026-04-18 11:21:46 -0300

    feat(db): establish connection to postgres container

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,18 +7,38 @@ import (
 	"os"
 
 	"github.com/CasimiroDev/volunteer-service/internal/handler"
+	"github.com/CasimiroDev/volunteer-service/internal/repository"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Aviso: Arquivo .env não encontrado.")
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		log.Fatal("Erro crítico: DATABASE_URL não está configurada.")
+	}
+
+	fmt.Println("Conectando ao banco de dados...")
+	db, err := repository.NewDatabaseConnection(dbURL)
+	if err != nil {
+		log.Fatalf("Erro crítico: falha ao iniciar o banco de dados: %v", err)
+	}
+	defer db.Close()
+	fmt.Println("Conexão com o PostgreSQL estabelecida com sucesso!")
+
+	mux := http.NewServeMux()
 	mux.HandleFunc("GET /health", handler.HealthCheck)
 
-	port := os.Getenv("PORT")
 	fmt.Printf("Servidor Gestão de Voluntários iniciado na porta %s...\n", port)
-
-	err := http.ListenAndServe(":"+port, mux)
-	if err != nil {
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
 		log.Fatalf("Erro crítico no servidor: %v", err)
 	}
 }
